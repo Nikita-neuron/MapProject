6a5f599962f98db75f2cc5ba6524646334a454a0
Nikita-neuron <64783326+Nikita-neuron@users.noreply.github.com>
2021-04-30 11:06:22 +0300

    add user location

--- app/src/main/java/com/example/mapproject/MainActivity.java
+++ app/src/main/java/com/example/mapproject/MainActivity.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Point;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -39,11 +41,22 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000, 5, this);
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
-        getLocation();
     }
 
     @Override
@@ -54,10 +67,10 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         LatLng moscow = new LatLng(55.754724, 37.621380);
-        mMap.addMarker(new MarkerOptions()
-                .position(moscow)
-                .title("Marker in Moscow"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//        mMap.addMarker(new MarkerOptions()
+//                .position(moscow)
+//                .title("Marker in Moscow"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
 
     }
@@ -76,9 +89,17 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onLocationChanged(@NonNull Location location) {
         LatLng latLng= new LatLng(location.getLatitude(), location.getLongitude());
 
+        System.out.println(location.getLatitude() + " " + location.getLongitude());
+
+        mMap.clear();
+
         mMap.addMarker(new MarkerOptions()
                 .position(latLng)
                 .title("Marker in Moscow"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+
+        Point point = new Point();
+
+        mMap.animateCamera(CameraUpdateFactory.zoomBy(5, mMap.getProjection().toScreenLocation(latLng)));
     }
 }
